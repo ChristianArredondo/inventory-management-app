066210e75f63561b0942dc0957feacb56f83f128
christianarredondo <christianarredondo@utexas.edu>
2017-07-12 07:56:40 -0500

    Finally add dynamically-clicked class to product row

--- src/app/products-list/products-list.component.ts
+++ src/app/products-list/products-list.component.ts
@@ -21,10 +21,18 @@ export class ProductsListComponent implements OnInit {
   ngOnInit() {
   }
   clicked(product: Product) {
-    console.log('Product hath been clickethed! ');
-    console.log(product);
+    // console.log('Product hath been clickethed! ');
+    // console.log(product);
     this.currentProduct = product;
     this.onProductSelected.emit(product);
   }
 
+  isSelected(product: Product) {
+    console.log('Im in! Argument passed = ');
+    console.log(product);
+    if (!product || !this.currentProduct) {
+      return false
+    }
+    return product.sku === this.currentProduct.sku;
+  }
 }
