525e90daeeac22e10f3e9602f315c62370993146
Christian Arredondo <carredondo@lja.com>
2017-08-06 07:31:37 -0500

    cosmetic changes - hr, color, and item descriptions

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,25 +13,25 @@ export class AppComponent {
   constructor() {
     this.products = [
       new Product(
-        'NICEHAT',
-        'Hook em horns!',
+        'HOOKEMHORNS',
+        'Longhorns Hat',
         'http://images.footballfanatics.com/FFImage/thumb.aspx?i=/productImages/_2439000/ff_2439751_full.jpg&w=340',
         ['Men', 'Accessories', 'Hats'],
-        29.99
+        19.99
       ),
       new Product(
         'SHOES',
-        'Teal-sh Running shoes',
+        'Running shoes',
         'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR4wkxwtl4KrFvlk9W_EPuCyRGvy6ZqKH6uHAd0BA8UarfDo-SRSA',
         ['Men', 'Shoes', 'Running Shoes'],
-        29.99
+        89.99
       ),
       new Product(
         'NEATOJACKET',
-        'A very neat and warm jacket',
+        'Comfy Jacket',
         'http://dimg.dillards.com/is/image/DillardsZoom/03709447_zi_alaskan_heather?$ecom308x357$',
         ['Men', 'Apparel', 'Jackets and Vests'],
-        29.99
+        59.99
       )
     ];
   }
